c79bd84d6b844a0e3c8a6d2102029cd2e1c27045
Abhishek Kr <ak96166956@gmail.com>
2025-04-03 03:02:49 +0530

    add doctor

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+      extend: {
+        colors: {
+          // primary: "#1f1f1f",
+          secondary: "#f0f0f0",
+        }
+        
+      },
+    },
+    plugins: [],
+  }
+  
